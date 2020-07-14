8cd9608ff041149cf077c93e47d2ab2915b84521
Artur Klesun <arturklesun@gmail.com>
2020-07-15 01:03:04 +0300

    And one more place

--- src/client/CreateLobby.js
+++ src/client/CreateLobby.js
@@ -4,7 +4,7 @@ import Input from "./Input.js";
 import Select from "./Select.js";
 import {AI_BEHAVIOUR, PLAYER_CODE_NAMES} from "../Constants.js";
 
-let form, modal;
+let modal;
 
 const getBody = () => {
     const columns = [
@@ -21,65 +21,53 @@ const getBody = () => {
         }
     ];
 
-    form = Dom('form', {}, [
+    return Dom('form', {}, [
         Dom('div', {
             class: 'col-grid',
             style: 'min-width: 465px',
-        }, columns.map( col => {
-            const row = Dom('div', {class: 'row-grid a-i-center col-space-sm'});
-            const label = Dom('label', {class: 'form-label'});
-            label.innerHTML = col.label;
-
-            const leftCol = Dom('div', {class: 'col-grid flex-sm'});
-            leftCol.appendChild(label);
-
-            const rightCol = Dom('div', {class: 'col-grid flex-sm'});
-            rightCol.appendChild(col.input);
-
-            row.appendChild(leftCol);
-            row.appendChild(rightCol);
-
-            return row;
-        } )),
+        }, columns.map( col => Dom('div', {
+            class: 'row-grid a-i-center col-space-sm',
+        }, [
+            Dom('div', {class: 'col-grid flex-sm'}, [
+                Dom('label', {class: 'form-label'}, col.label),
+            ]),
+            Dom('div', {class: 'col-grid flex-sm'}, [col.input]),
+        ]))),
         Dom('div', {class: 'col-grid'}, [
             Dom('div', {
                 class: 'row-grid a-i-center col-space-sm j-c-sb no-gutters-side-md',
-            }, PLAYER_CODE_NAMES.map( (player, i) => {
-                return Dom('div', {
-                    class: 'col-grid player-col m-r-md m-l-md'
+            }, PLAYER_CODE_NAMES.map( (player, i) => Dom('div', {
+                class: 'col-grid player-col m-r-md m-l-md'
+            }, [
+                Dom('div', {
+                    class: `player-container ${player.toLocaleLowerCase()}`,
+                    'data-owner': player,
                 }, [
-                    Dom('div', {
-                        class: `player-container ${player.toLocaleLowerCase()}`,
-                        'data-owner': player,
-                    }, [
-                        Dom('div', {class: 'player-title'}, player),
-                        Select({
-                            name: 'aiBase',
-                            wrapperClass: player.toLocaleLowerCase(),
-                            options: [
-                                {value: AI_BEHAVIOUR.skipTurns, label: "No AI (Skip Turns)"},
-                                {value: AI_BEHAVIOUR.pureRandom, label: "Pure Random"},
-                                {value: AI_BEHAVIOUR.leasRecentTiles, label: "Least Recent Tiles"},
-                                {value: AI_BEHAVIOUR.resourcePathfinding, label: "Resource Pathfinding"},
-                            ]
-                        }).context,
-                        Dom('label', {class: 'allow-player'}, [
-                            Dom('span'),
-                            Dom('input', {
-                                type: 'checkbox',
-                                name: 'allowHuman',
-                            }, "Allow Players to Join")
-                        ]),
+                    Dom('div', {class: 'player-title'}, player),
+                    Select({
+                        name: 'aiBase',
+                        wrapperClass: player.toLocaleLowerCase(),
+                        options: [
+                            {value: AI_BEHAVIOUR.skipTurns, label: "No AI (Skip Turns)"},
+                            {value: AI_BEHAVIOUR.pureRandom, label: "Pure Random"},
+                            {value: AI_BEHAVIOUR.leasRecentTiles, label: "Least Recent Tiles"},
+                            {value: AI_BEHAVIOUR.resourcePathfinding, label: "Resource Pathfinding"},
+                        ]
+                    }).context,
+                    Dom('label', {class: 'allow-player'}, [
+                        Dom('span'),
+                        Dom('input', {
+                            type: 'checkbox',
+                            name: 'allowHuman',
+                        }, "Allow Players to Join")
                     ]),
-                ]);
-            } )),
+                ]),
+            ]))),
         ]),
     ]);
-
-    return form;
 };
 
-const getActions = (api, reloadGame) => {
+const getActions = (api, reloadGame, form) => {
     return Dom('div', {class: 'row-grid j-c-right'}, [
         Dom('button', {
             class: 'form-btn',
@@ -108,10 +96,11 @@ const getActions = (api, reloadGame) => {
 };
 
 const CreateLobbyDialog = (api, reloadGame) => {
+    const form = getBody();
     const config = {
         title: 'CREATE LOBBY',
-        body: getBody(),
-        actions: getActions(api, reloadGame),
+        body: form,
+        actions: getActions(api, reloadGame, form),
     };
 
     modal = createDialog(config);
