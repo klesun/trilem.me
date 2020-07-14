1df1323e1e2b55521a6b3ddd745fbf4da202466b
Artur Klesun <arturklesun@gmail.com>
2020-07-15 00:51:15 +0300

    Refactor CreateLobby.js - pass children with third argument of Dom() instead of appendChild()

--- src/client/CreateLobby.js
+++ src/client/CreateLobby.js
@@ -21,67 +21,60 @@ const getBody = () => {
         }
     ];
 
-    form = Dom('form');
-    const mainRow = Dom('div', {class: 'col-grid', style: 'min-width: 465px'});
+    form = Dom('form', {}, [
+        Dom('div', {
+            class: 'col-grid',
+            style: 'min-width: 465px',
+        }, columns.map( col => {
+            const row = Dom('div', {class: 'row-grid a-i-center col-space-sm'});
+            const label = Dom('label', {class: 'form-label'});
+            label.innerHTML = col.label;
 
-    const rows = columns.map( col => {
-        const row = Dom('div', {class: 'row-grid a-i-center col-space-sm'});
-        const label = Dom('label', {class: 'form-label'});
-        label.innerHTML = col.label;
+            const leftCol = Dom('div', {class: 'col-grid flex-sm'});
+            leftCol.appendChild(label);
 
-        const leftCol = Dom('div', {class: 'col-grid flex-sm'});
-        leftCol.appendChild(label);
+            const rightCol = Dom('div', {class: 'col-grid flex-sm'});
+            rightCol.appendChild(col.input);
 
-        const rightCol = Dom('div', {class: 'col-grid flex-sm'});
-        rightCol.appendChild(col.input);
+            row.appendChild(leftCol);
+            row.appendChild(rightCol);
 
-        row.appendChild(leftCol);
-        row.appendChild(rightCol);
-
-        return row;
-    } );
-
-    rows.forEach( row => mainRow.appendChild(row) );
-
-    const secondaryRow = Dom('div', {class: 'col-grid'});
-    const secondaryRow2 = Dom('div', {class: 'row-grid a-i-center col-space-sm j-c-sb no-gutters-side-md'});
-
-    PLAYER_CODE_NAMES.forEach( (player, i) => {
-        const col = Dom('div', {class: 'col-grid player-col m-r-md m-l-md'});
-        const cube = Dom('div', {class: `player-container ${player.toLocaleLowerCase()}`})
-        cube.setAttribute('data-owner', player);
-        const title = Dom('div', {class: 'player-title'});
-        title.innerText = player;
-        const sel = Select({
-            name: 'aiBase',
-            wrapperClass: player.toLocaleLowerCase(),
-            options: [
-                {value: AI_BEHAVIOUR.skipTurns, label: "No AI (Skip Turns)"},
-                {value: AI_BEHAVIOUR.pureRandom, label: "Pure Random"},
-                {value: AI_BEHAVIOUR.leasRecentTiles, label: "Least Recent Tiles"},
-                {value: AI_BEHAVIOUR.resourcePathfinding, label: "Resource Pathfinding"},
-            ]
-        }).context;
-
-        const cb = Dom('label', {class: 'allow-player'});
-        const cbText = Dom('span');
-        const cbInput = Dom('input', {type: 'checkbox', name: 'allowHuman'});
-        cbText.innerText = "Allow Players to Join"
-
-        cb.appendChild(cbText);
-        cb.appendChild(cbInput);
-
-        cube.appendChild(title);
-        cube.appendChild(sel);
-        cube.appendChild(cb);
-
-        col.appendChild(cube);
-        secondaryRow2.appendChild(col);
-    } );
-
-    secondaryRow.appendChild(secondaryRow2);
-    form.appendChild(mainRow);
-    form.appendChild(secondaryRow);
+            return row;
+        } )),
+        Dom('div', {class: 'col-grid'}, [
+            Dom('div', {
+                class: 'row-grid a-i-center col-space-sm j-c-sb no-gutters-side-md',
+            }, PLAYER_CODE_NAMES.map( (player, i) => {
+                return Dom('div', {
+                    class: 'col-grid player-col m-r-md m-l-md'
+                }, [
+                    Dom('div', {
+                        class: `player-container ${player.toLocaleLowerCase()}`,
+                        'data-owner': player,
+                    }, [
+                        Dom('div', {class: 'player-title'}, player),
+                        Select({
+                            name: 'aiBase',
+                            wrapperClass: player.toLocaleLowerCase(),
+                            options: [
+                                {value: AI_BEHAVIOUR.skipTurns, label: "No AI (Skip Turns)"},
+                                {value: AI_BEHAVIOUR.pureRandom, label: "Pure Random"},
+                                {value: AI_BEHAVIOUR.leasRecentTiles, label: "Least Recent Tiles"},
+                                {value: AI_BEHAVIOUR.resourcePathfinding, label: "Resource Pathfinding"},
+                            ]
+                        }).context,
+                        Dom('label', {class: 'allow-player'}, [
+                            Dom('span'),
+                            Dom('input', {
+                                type: 'checkbox',
+                                name: 'allowHuman',
+                            }, "Allow Players to Join")
+                        ]),
+                    ]),
+                ]);
+            } )),
+        ]),
+    ]);
 
     return form;
 };
